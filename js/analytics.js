b6e3f4f8814eed8adcc07cf7bb109418e3a06d4a
SilasReinagel <silas.reinagel@gmail.com>
2019-04-03 16:47:41 -0700

    Added Google Analytics. Closes #8

---
+++ js/analytics.js
@@ -0,0 +1,9 @@
+if(window.location.hostname !== "localhost")
+{
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag(){dataLayer.push(arguments);}
+
+    gtag('js', new Date());
+    gtag('config', 'UA-93176410-3');
+}
